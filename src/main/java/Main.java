8677cbaafe24e437f0d990b12e5a54270a94d952
Haroun <haroundominique@gmail.com>
2022-11-13 15:40:21 +0100

    Making the program a little more sophisticated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Introduce un número:");
         Scanner scan = new Scanner(System.in);
-        long numero = scan.nextLong();
+        long number = scan.nextLong();
+        long aux = number;
 
-        while (true) {
-            if (numero % 2 == 0) {
-                numero = numero / 2;
+        ArrayList<Long> tail = new ArrayList<>();
+
+        for (long i=0; i<aux*3; i++) {
+            if (number % 2 == 0) {
+                number = number / 2;
             } else {
-                numero = (numero * 3) + 1;
+                number = (number * 3) + 1;
             }
 
-            System.out.println(numero);
+            //System.out.println(number);
+
+            if(i>=(aux*3)-3){
+                tail.add(number);
+            }
+
+        }
+        for(Long l : tail){
+            System.out.println(l);
         }
     }
 }
